7ad3ca840c0fad1be1491244fee864a1f86012ea
Pizzi Andrea <andreapizzi90@outlook.com>
2021-04-05 14:23:48 +0200

    min modifica

--- aggiungi.php
+++ aggiungi.php
@@ -4,13 +4,20 @@
   if(!$db->connect_errno){
     die("<h1>Connesione fallita</h1>");
   }
+  if ($_POST[ID_Sq] == ''){
+    $query = "INSERT INTO ATLETI VALUES("."'$_POST[ID_A]',"."'$_POST[Nome_A]',"."'$_POST[Cognome_A]',"."'$_POST[Data_N]',"."'$_POST[Istituto]',"."'$_POST[Nazionalità_A]',"."NULL);";
 
-  $query = "INSERT INTO ATLETI VALUES("."'$_POST[ID_A]',"."'$_POST[Nome_A]',"."'$_POST[Cognome_A]',"."'$_POST[Data_N]',"."'$_POST[Istituto]',"."'$_POST[Nazionalità_A]',"."'$_POST[ID_Sq]');";
+  }
+  else
+  {
+    $query = "INSERT INTO ATLETI VALUES("."'$_POST[ID_A]',"."'$_POST[Nome_A]',"."'$_POST[Cognome_A]',"."'$_POST[Data_N]',"."'$_POST[Istituto]',"."'$_POST[Nazionalità_A]',"."'$_POST[ID_Sq]');";
 
+  }
+  
   if( $db->query( $query ) )
     echo "Registrazione Eseguita";
   else {
-    echo "Errore inaspettato, impossibile registrare nuovo utente";
+    echo "Errore inaspettato, impossibile registrare nuovo utente<br>";
     echo $query;
   }
     
