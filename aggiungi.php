7a7dcc4412d7c4ca0210d01372d574b25f489b81
Pizzi Andrea <andreapizzi90@outlook.com>
2021-04-05 13:30:27 +0200

    Risuluzione problema con query

--- aggiungi.php
+++ aggiungi.php
@@ -5,13 +5,15 @@
     die("<h1>Connesione fallita</h1>");
   }
 
-  $query = "INSERT INTO ATLETI VALUES(".$_POST['ID_A'].','.$_POST['Nome_A'].','.$_POST['Cognome_A'].','.$_POST['Data_N'].','.$_POST['Istituto'].','.$_POST['Nazionalità_A'].','.$_POST['ID_Sq'].');';
+  $query = "INSERT INTO ATLETI VALUES("."'$_POST[ID_A]',"."'$_POST[Nome_A]',"."'$_POST[Cognome_A]',"."'$_POST[Data_N]',"."'$_POST[Istituto]',"."'$_POST[Nazionalità_A]',"."'$_POST[ID_Sq]');";
 
   if( $db->query( $query ) )
     echo "Registrazione Eseguita";
-  else
+  else {
     echo "Errore inaspettato, impossibile registrare nuovo utente";
-
+    echo $query;
+  }
+    
   $db->close();
 
 ?>
