894c3899224549c638537eda202b4650adc91e66
Pizzi Andrea <andreapizzi90@outlook.com>
2021-04-06 00:11:52 +0200

    Sistemazione della sezione inserisci risultato

--- ControlPanel/RegistraPunteggio/registra.php
+++ ControlPanel/RegistraPunteggio/registra.php
@@ -4,7 +4,7 @@
     if($db->connect_errno){
         die("ERRORE, database non disponibile");
     }
-
+    
     $query = "INSERT INTO PARTECIPANO_A VALUES("."'$_POST[ID_A]',"."'$_POST[ID_G]',"."'$_POST[Punteggio_A]',"."'$_POST[Posizione_A]');";
 
     if( !$db->query($query) ){
--- ControlPanel/RegistraPunteggio/registraS.php
+++ ControlPanel/RegistraPunteggio/registraS.php
@@ -1,15 +1,19 @@
 <?php 
-    $db = new mysqli('localhost', 'root', 'pass', 'GARE_SPORTIVE');
+    $db = new mysqli('localhost', 'root', 'pass', 'GARE_INTERNAZIONALI');
 
     if( $db->connect_errno ){
         die("Impossibile stabilire una connesione");
     }
-
+    $idsq = $_POST[ID_Sqx];
+    $idg = $_POST[ID_Gx];
+    $psq = $_POST[Punteggio_Sqx];
+    $possq = $_POST[Posizione_Sqx];
     $query = "INSERT INTO PARTECIPANO_S
-              VALUES('".$_POST[ID_Sq]."','".$_POST[ID_G]."','".$_POST[Punteggio_Sq]."','".$_POST[Posizione_Sq].");";
+              VALUES( '$idsq', '$idg', '$psq', '$possq' );";
     
     if( !$db->query( $query ) ){
-        echo "<h2>Impossibile completare la registrazione...</h2>";
+        echo "<h2>Impossibile completare la registrazione</h2>";
+        echo $query;
     }
 
     $db->close();
